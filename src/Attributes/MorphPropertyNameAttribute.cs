5672e586454b8285be76e7cdea37149626cad2c6
SimonNyvall <simon.k.nyvall@proton.me>
2025-04-05 14:57:56 +0200

    feat: morph extension on object class

---
+++ src/Attributes/MorphPropertyNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Morphiq.Attributes
+{
+    public class MorphPropertyNameAttribute : Attribute
+    {
+        public string TargetName { get; }
+
+        public MorphPropertyNameAttribute(string targetName)
+        {
+            TargetName = targetName;
+        }
+    }    
+}
+
---
+++ src/MorphExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reflection;
+using Morphiq.Attributes;
+
+namespace Morphiq
+{
+    public static class MorphExtensions
+    {
+        public static ToType MorphTo<ToType>(this object fromObj) where ToType : new()
+        {
+            var toObject = new ToType();
+            var fromProperties = fromObj.GetType().GetProperties();
+            var toProperties = typeof(ToType).GetProperties();
+
+            foreach (var fromProperty in fromProperties)
+            {
+                var targetName = fromProperty.Name;
+                var attribute = fromProperty.GetCustomAttribute<MorphPropertyNameAttribute>();
+
+                if (attribute != null && !string.IsNullOrEmpty(attribute.TargetName))
+                {
+                    targetName = attribute.TargetName;
+                }
+
+                var toProperty = toProperties.FirstOrDefault(x => x.Name == targetName);
+
+                if (toProperty != null && toProperty.CanWrite)
+                {
+                    toProperty.SetValue(toObject, fromProperty.GetValue(fromObj));
+                }
+            }
+
+            return toObject;
+        }
+    }
+}
+
